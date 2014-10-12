93721d505ef0d03d6fd220a5d77cd465b0ca81f6
aldilaff <aldilaff@gmail.com>
2014-10-11 21:13:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aldilaff/helloanroid/MyActivity.java
@@ -0,0 +1,46 @@
+package com.example.aldilaff.helloanroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void helloWorld(View view){
+        EditText inputfield = (EditText) findViewById(R.id.inputfield);
+        String inputfieldText = inputfield.getText().toString();
+        Toast.makeText(getApplicationContext(),inputfieldText,Toast.LENGTH_LONG).show();
+    }
+}
